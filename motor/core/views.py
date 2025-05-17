3d8f5dd2f11382ba461de839d80377e3a5f168dc
Cix <gabrielviniciodacosta@gmail.com>
2025-05-17 11:28:43 -0300

    #

--- motor/core/views.py
+++ motor/core/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def contato (request):
+    context = {
+        'nome':'MotorWeb',
+        'fone':'17-123456789',
+        'email':'email@teste.com',
+    }
+    return render(request, 'contato.html', context)
